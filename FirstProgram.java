170f9e8598a3ffafe6a8efe8f4c6be4b09814d9f
Ojas Joshi <joshiojas185@gmail.com>
2025-09-24 14:29:03 +0530

    Cganges in first program

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,5 +1,6 @@
 public class FirstProgram{
     public static void main(String[] args) {
         System.out.println("Hello, This is my first Program");
+        System.out.println("New line added");
     }
 }
